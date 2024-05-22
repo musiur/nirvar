8b7e1acd9451af5798913ba4f09c627f61e3e3f1
musiur <musiur.opu@gmail.com>
2024-05-22 17:00:10 +0600

    auth layout py-10 added

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 
 const AuthLayout = ({ children }: { children: ReactElement }) => {
-  return <div className="min-h-[70dvh] pt-10">{children}</div>;
+  return <div className="min-h-[100dvh] py-10">{children}</div>;
 };
 
 export default AuthLayout;
